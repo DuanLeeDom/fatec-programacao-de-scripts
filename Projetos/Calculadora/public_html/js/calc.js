af819a09207ca66c6014d0f10767421b6c89f1c8
Duan <116463153+DuanLeeDom@users.noreply.github.com>
2025-02-21 09:34:05 -0300

    Add files via upload

---
+++ Projetos/Calculadora/public_html/js/calc.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// barra  barra é utilizado para criar um comentário de uma linha
+
+// função para exibir uma janela de aviso.
+
+window.alert('Bem-vindo a calculadora feito em javascript!');
+
+let hora = new Date();
+
+document.getElementById("relogio").innerHTML = hora;
